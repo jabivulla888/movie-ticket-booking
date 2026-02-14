f2c7a1369c9e9f214306cc2389c3004482bc8725
jabivulla888 <jabivulla888@gmail.com>
2026-02-14 19:27:04 +0530

    Update ShowController.java

--- show-service/src/main/java/com/xyz/show/controller/ShowController.java
+++ show-service/src/main/java/com/xyz/show/controller/ShowController.java
@@ -1,1 +1,32 @@
+package com.xyz.show.controller;
 
+import com.xyz.show.model.Show;
+import com.xyz.show.service.ShowService;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/shows")
+public class ShowController {
+
+    private final ShowService showService;
+
+    public ShowController(ShowService showService) {
+        this.showService = showService;
+    }
+
+    @GetMapping
+    public List<Show> browseShows(
+            @RequestParam Long movieId,
+            @RequestParam String city,
+            @RequestParam String date) {
+
+        return showService.getShows(
+                movieId,
+                city,
+                LocalDate.parse(date)
+        );
+    }
+}
