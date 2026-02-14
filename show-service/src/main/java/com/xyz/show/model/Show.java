7c193b86dd18ab5074ab46068437e437bf7efea2
jabivulla888 <jabivulla888@gmail.com>
2026-02-14 19:34:42 +0530

    Update Show.java

--- show-service/src/main/java/com/xyz/show/model/Show.java
+++ show-service/src/main/java/com/xyz/show/model/Show.java
@@ -1,21 +1,30 @@
 package com.xyz.show.model;
 
+import jakarta.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+@Entity
+@Table(name = "shows")
 public class Show {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private Long movieId;
     private String theatreName;
     private String city;
+
     private LocalDate showDate;
     private LocalTime showTime;
+
     private int availableSeats;
 
-    public Show(Long id, Long movieId, String theatreName, String city,
+    public Show() {}
+
+    public Show(Long movieId, String theatreName, String city,
                 LocalDate showDate, LocalTime showTime, int availableSeats) {
-        this.id = id;
         this.movieId = movieId;
         this.theatreName = theatreName;
         this.city = city;
@@ -24,31 +33,11 @@ public class Show {
         this.availableSeats = availableSeats;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public Long getMovieId() {
-        return movieId;
-    }
-
-    public String getTheatreName() {
-        return theatreName;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public LocalDate getShowDate() {
-        return showDate;
-    }
-
-    public LocalTime getShowTime() {
-        return showTime;
-    }
-
-    public int getAvailableSeats() {
-        return availableSeats;
-    }
+    public Long getId() { return id; }
+    public Long getMovieId() { return movieId; }
+    public String getTheatreName() { return theatreName; }
+    public String getCity() { return city; }
+    public LocalDate getShowDate() { return showDate; }
+    public LocalTime getShowTime() { return showTime; }
+    public int getAvailableSeats() { return availableSeats; }
 }
