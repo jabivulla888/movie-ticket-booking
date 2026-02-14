003868795d8c21f6d4626f085069def6e95b1148
jabivulla888 <jabivulla888@gmail.com>
2026-02-14 18:33:22 +0530

    Update ShowController.java

--- show-service/controller/ShowController.java
+++ show-service/controller/ShowController.java
@@ -1,1 +1,18 @@
+@RestController
+@RequestMapping("/api/shows")
+public class ShowController {
 
+    private final ShowService showService;
+
+    public ShowController(ShowService showService) {
+        this.showService = showService;
+    }
+
+    @GetMapping
+    public List<ShowResponse> getShows(
+            @RequestParam Long movieId,
+            @RequestParam String city,
+            @RequestParam LocalDate date) {
+        return showService.getShows(movieId, city, date);
+    }
+}
