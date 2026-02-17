55e8d596dcde42c60c17d1e66bc1df7b31e287d3
jabivulla888 <jabivulla888@gmail.com>
2026-02-17 20:08:21 +0530

    Update ShowApplication.java

--- show-service/src/main/java/com/xyz/show/ShowApplication.java
+++ show-service/src/main/java/com/xyz/show/ShowApplication.java
@@ -1,1 +1,14 @@
+package com.xyz.show;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@EnableCaching
+@SpringBootApplication
+
+public class ShowApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShowApplication.class, args);
+    }
+}
