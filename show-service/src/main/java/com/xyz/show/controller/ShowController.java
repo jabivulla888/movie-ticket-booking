8e9da21b309fab885be8277fb8d4c045a0c900e8
jabivulla888 <jabivulla888@gmail.com>
2026-02-17 20:13:33 +0530

    Update ShowController.java

--- show-service/src/main/java/com/xyz/show/controller/ShowController.java
+++ show-service/src/main/java/com/xyz/show/controller/ShowController.java
@@ -2,10 +2,10 @@ package com.xyz.show.controller;
 
 import com.xyz.show.model.Show;
 import com.xyz.show.service.ShowService;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/shows")
@@ -18,15 +18,19 @@ public class ShowController {
     }
 
     @GetMapping
-    public List<Show> browseShows(
+    public Page<Show> browseShows(
             @RequestParam Long movieId,
             @RequestParam String city,
-            @RequestParam String date) {
+            @RequestParam String date,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
 
         return showService.getShows(
                 movieId,
                 city,
-                LocalDate.parse(date)
+                LocalDate.parse(date),
+                page,
+                size
         );
     }
 }
