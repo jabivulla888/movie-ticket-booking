07038acdb5c2ae33e9577bce9870a7989fa47917
jabivulla888 <jabivulla888@gmail.com>
2026-02-17 20:34:01 +0530

    Update SeatRepository.java

--- booking-service/src/main/java/com/xyz/booking/repository/SeatRepository.java
+++ booking-service/src/main/java/com/xyz/booking/repository/SeatRepository.java
@@ -1,1 +1,19 @@
+package com.xyz.booking.repository;
 
+import com.xyz.booking.model.Seat;
+import com.xyz.booking.model.SeatStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface SeatRepository extends JpaRepository<Seat, Long> {
+
+    List<Seat> findByShowId(Long showId);
+
+    List<Seat> findByShowIdAndStatus(Long showId, SeatStatus status);
+
+    Optional<Seat> findByShowIdAndSeatNumber(Long showId, String seatNumber);
+}
