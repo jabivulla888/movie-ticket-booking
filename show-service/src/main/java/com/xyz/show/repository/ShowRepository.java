6396cdbe59fc2b56fa6fcb9d986097838724d897
jabivulla888 <jabivulla888@gmail.com>
2026-02-17 20:03:35 +0530

    Update ShowRepository.java

--- show-service/src/main/java/com/xyz/show/repository/ShowRepository.java
+++ show-service/src/main/java/com/xyz/show/repository/ShowRepository.java
@@ -1,1 +1,18 @@
+package com.xyz.show.repository;
 
+import com.xyz.show.model.Show;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+
+public interface ShowRepository extends JpaRepository<Show, Long> {
+
+    Page<Show> findByMovieIdAndCityIgnoreCaseAndShowDate(
+            Long movieId,
+            String city,
+            LocalDate showDate,
+            Pageable pageable
+    );
+}
